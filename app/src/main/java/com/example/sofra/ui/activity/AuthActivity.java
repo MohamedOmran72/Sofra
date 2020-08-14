883f444b68a081f719d92572088725121d3227f0
Mohamed Omran <mohamed_omran72@outlook.com>
2020-08-14 07:07:26 +0200

    feat: add login screen design

---
+++ app/src/main/java/com/example/sofra/ui/activity/AuthActivity.java
@@ -0,0 +1,14 @@
+package com.example.sofra.ui.activity;
+
+import android.os.Bundle;
+
+import com.example.sofra.R;
+
+public class AuthActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
---
+++ app/src/main/java/com/example/sofra/ui/fragment/LoginFragment.java
@@ -0,0 +1,18 @@
+package com.example.sofra.ui.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.sofra.R;
+
+public class LoginFragment extends BaseFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+}
