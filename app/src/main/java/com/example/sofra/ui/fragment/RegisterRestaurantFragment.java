93d42113314862debcd353bbf6d425696c8d4dba
Mohamed Omran <mohamed_omran72@outlook.com>
2020-08-16 05:58:06 +0200

    feat: add register screen design

---
+++ app/src/main/java/com/example/sofra/ui/fragment/RegisterRestaurantFragment.java
@@ -0,0 +1,22 @@
+package com.example.sofra.ui.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.sofra.R;
+
+class RegisterRestaurantFragment extends BaseFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_register_restaurant, container, false);
+    }
+}
