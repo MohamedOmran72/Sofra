c6cd3d2d52c752ad3a0881ffa1ae32c5cf3ee176
Mohamed Omran <mohamed_omran72@outlook.com>
2020-12-26 00:10:34 +0200

    feat: add Navigation splash screen

---
+++ app/src/main/java/com/example/sofra/SplashFragment.java
@@ -0,0 +1,24 @@
+package com.example.sofra;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class SplashFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false);
+    }
+}
--- app/src/main/java/com/example/sofra/ui/activity/SplashActivity.java
+++ app/src/main/java/com/example/sofra/ui/activity/SplashActivity.java
@@ -1,65 +1,63 @@
 package com.example.sofra.ui.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-import com.example.sofra.databinding.ActivitySplashBinding;
+import androidx.appcompat.app.AppCompatActivity;
 
-import static com.example.sofra.data.local.SharedPreferencesManger.LoadData;
+import com.example.sofra.R;
 
-public class SplashActivity extends BaseActivity {
+public class SplashActivity extends AppCompatActivity {
 
-    // add viewBinding reference to use
-    private ActivitySplashBinding binding;
+//    // add viewBinding reference to use
+//    private ActivitySplashBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // if the user login go to Home direct
-        // else wait user chosen
-        if (LoadData(this, "userType") != null) {
-            Intent intent = new Intent(this, HomeActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
-            finish();
-        }
+//        // if the user login go to Home direct
+//        // else wait user chosen
+//        if (LoadData(this, "userType") != null) {
+//            Intent intent = new Intent(this, HomeActivity.class);
+//            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//            startActivity(intent);
+//            finish();
+//        }
 
         super.onCreate(savedInstanceState);
-        // inflate Layout with viewBinding
-        binding = ActivitySplashBinding.inflate(getLayoutInflater());
-        View view = binding.getRoot();
-        setContentView(view);
+//        // inflate Layout with viewBinding
+//        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+//        View view = binding.getRoot();
+        setContentView(R.layout.activity_splash);
     }
 
-    /**
-     * Dispatch onResume() to fragments.  Note that for better inter-operation
-     * with older versions of the platform, at the point of this call the
-     * fragments attached to the activity are <em>not</em> resumed.
-     */
-    @Override
-    protected void onResume() {
-        super.onResume();
-        binding.splashActivityButtonOrderFood.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
-                intent.putExtra("userType", "client");
-                startActivity(intent);
-            }
-        });
-
-        binding.splashActivityButtonSellFood.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(SplashActivity.this, AuthActivity.class);
-                intent.putExtra("userType", "seller");
-                startActivity(intent);
-            }
-        });
-    }
-
-    @Override
-    public void onBackPressed() {
-        System.exit(0);
-    }
+//    /**
+//     * Dispatch onResume() to fragments.  Note that for better inter-operation
+//     * with older versions of the platform, at the point of this call the
+//     * fragments attached to the activity are <em>not</em> resumed.
+//     */
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        binding.splashActivityButtonOrderFood.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
+//                intent.putExtra("userType", "client");
+//                startActivity(intent);
+//            }
+//        });
+//
+//        binding.splashActivityButtonSellFood.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(SplashActivity.this, AuthActivity.class);
+//                intent.putExtra("userType", "seller");
+//                startActivity(intent);
+//            }
+//        });
+//    }
+//
+//    @Override
+//    public void onBackPressed() {
+//        System.exit(0);
+//    }
 }
