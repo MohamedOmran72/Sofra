904e3d274c6ce54907c3088d8605c90896236021
Mohamed Omran <mohamed_omran72@outlook.com>
2020-12-31 04:01:09 +0200

    feat: add Navigation from NewPasswordFragment to LoginFragment after reset password

--- app/src/main/java/com/example/sofra/ui/fragment/forgotPassword/NewPasswordFragment.java
+++ app/src/main/java/com/example/sofra/ui/fragment/forgotPassword/NewPasswordFragment.java
@@ -8,21 +8,17 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
 import com.example.sofra.R;
-import com.example.sofra.data.pojo.client.forgetPassword.ResetPassword;
 import com.example.sofra.databinding.FragmentNewPasswordBinding;
-import com.example.sofra.ui.fragment.login.LoginFragment;
 
 import static com.example.sofra.data.local.SharedPreferencesManger.LoadData;
 import static com.example.sofra.utils.CheckInput.isEditTextSet;
-import static com.example.sofra.utils.HelperMethod.replaceFragment;
 
 public class NewPasswordFragment extends Fragment {
 
-    private static final String TAG = NewPasswordFragment.class.getName();
     private FragmentNewPasswordBinding binding;
     private ClientNewPasswordViewModel clientNewPasswordViewModel;
     private RestaurantNewPasswordViewModel restaurantNewPasswordViewModel;
@@ -39,33 +35,46 @@ public class NewPasswordFragment extends Fragment {
             userType = LoadData(requireActivity(), "userType");
         }
 
+
+        restaurantNewPasswordViewModel = new ViewModelProvider(this).get(RestaurantNewPasswordViewModel.class);
+        restaurantNewPasswordViewModel.restaurantNewPasswordMutableLiveData.observe(getViewLifecycleOwner(), resetPassword -> {
+            if (resetPassword.getStatus() == 1) {
+                Toast.makeText(requireActivity(), resetPassword.getMsg(), Toast.LENGTH_SHORT).show();
+                Navigation.findNavController(view).navigate(R.id.action_newPasswordFragment_to_loginFragment);
+            } else {
+                Toast.makeText(requireActivity(), resetPassword.getMsg(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        clientNewPasswordViewModel = new ViewModelProvider(this).get(ClientNewPasswordViewModel.class);
+        clientNewPasswordViewModel.clientNewPasswordMutableLiveData.observe(getViewLifecycleOwner(), resetPassword -> {
+            if (resetPassword.getStatus() == 1) {
+                Toast.makeText(requireActivity(), resetPassword.getMsg(), Toast.LENGTH_SHORT).show();
+                Navigation.findNavController(view).navigate(R.id.action_newPasswordFragment_to_loginFragment);
+            } else {
+                Toast.makeText(requireActivity(), resetPassword.getMsg(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
         return view;
     }
 
-    /**
-     * Called when the fragment is visible to the user and actively running.
-     * This is generally
-     * tied to {@link NewPasswordFragment#onResume() Activity.onResume} of the containing
-     * Activity's lifecycle.
-     */
+
     @Override
     public void onResume() {
         super.onResume();
 
         // verify code and change password
-        binding.fragmentNewPasswordButtonChangePassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        binding.fragmentNewPasswordButtonChangePassword.setOnClickListener(v -> {
 
-                if (isEditTextSet(binding.fragmentNewPasswordEditTextCode
-                        , binding.fragmentNewPasswordEditTextPassword
-                        , binding.fragmentNewPasswordEditTextConfirmPassword)) {
+            if (isEditTextSet(binding.fragmentNewPasswordEditTextCode
+                    , binding.fragmentNewPasswordEditTextPassword
+                    , binding.fragmentNewPasswordEditTextConfirmPassword)) {
 
-                    if (userType.equals("client")) {
-                        clientNewPassword();
-                    } else {
-                        restaurantNewPassword();
-                    }
+                if (userType.equals("client")) {
+                    clientNewPassword();
+                } else {
+                    restaurantNewPassword();
                 }
             }
         });
@@ -76,24 +85,7 @@ public class NewPasswordFragment extends Fragment {
         String password = String.valueOf(binding.fragmentNewPasswordEditTextPassword.getText()).trim();
         String passwordConfirmation = String.valueOf(binding.fragmentNewPasswordEditTextConfirmPassword.getText()).trim();
 
-        clientNewPasswordViewModel = new ViewModelProvider(this).get(ClientNewPasswordViewModel.class);
         clientNewPasswordViewModel.clientNewPassword(code, password, passwordConfirmation);
-        clientNewPasswordViewModel.clientNewPasswordMutableLiveData.observe(this, new Observer<ResetPassword>() {
-            @Override
-            public void onChanged(ResetPassword resetPassword) {
-                if (resetPassword.getStatus() == 1) {
-                    Toast.makeText(requireActivity(), resetPassword.getMsg(), Toast.LENGTH_SHORT).show();
-
-                    Bundle bundle = new Bundle();
-                    bundle.putString("userType", userType);
-                    replaceFragment(getParentFragmentManager()
-                            , requireActivity().findViewById(R.id.auth_activity_frame).getId()
-                            , new LoginFragment(), TAG, bundle);
-                } else {
-                    Toast.makeText(requireActivity(), resetPassword.getMsg(), Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
     }
 
     private void restaurantNewPassword() {
@@ -101,23 +93,6 @@ public class NewPasswordFragment extends Fragment {
         String password = String.valueOf(binding.fragmentNewPasswordEditTextPassword.getText()).trim();
         String passwordConfirmation = String.valueOf(binding.fragmentNewPasswordEditTextConfirmPassword.getText()).trim();
 
-        restaurantNewPasswordViewModel = new ViewModelProvider(this).get(RestaurantNewPasswordViewModel.class);
         restaurantNewPasswordViewModel.restaurantNewPassword(code, password, passwordConfirmation);
-        restaurantNewPasswordViewModel.restaurantNewPasswordMutableLiveData.observe(this, new Observer<ResetPassword>() {
-            @Override
-            public void onChanged(ResetPassword resetPassword) {
-                if (resetPassword.getStatus() == 1) {
-                    Toast.makeText(requireActivity(), resetPassword.getMsg(), Toast.LENGTH_SHORT).show();
-
-                    Bundle bundle = new Bundle();
-                    bundle.putString("userType", userType);
-                    replaceFragment(getParentFragmentManager()
-                            , requireActivity().findViewById(R.id.auth_activity_frame).getId()
-                            , new LoginFragment(), TAG, bundle);
-                } else {
-                    Toast.makeText(requireActivity(), resetPassword.getMsg(), Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
     }
 }
